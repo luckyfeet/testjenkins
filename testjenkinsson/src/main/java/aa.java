e41659d4b26a531899269c6651687844b6fec3b0
gh <2495186452@qq.com>
2021-12-14 11:27:05 +0800

    aa

---
+++ testjenkinsson/src/main/java/aa.java
@@ -0,0 +1,10 @@
+/**
+ * 包名:PACKAGE_NAME
+ * Description:
+ * 日期2021-12-14   11:26
+ */
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("hello jenkins");
+    }
+}
