b2de18ec810530657835eeb1292ad83ab3db9430
gh <2495186452@qq.com>
2021-12-14 15:36:18 +0800

    updated

--- testjenkinsson/src/main/java/aa.java
+++ testjenkinsson/src/main/java/aa.java
@@ -7,8 +7,6 @@ public class aa {
     public static void main(String[] args) {
         System.out.println("hello jenkins");
         System.out.println("day");
-        System.out.println("watermelon");
-        System.out.println("xigua");
-        System.out.println("gtxh");
+        System.out.println("pass");
     }
 }
