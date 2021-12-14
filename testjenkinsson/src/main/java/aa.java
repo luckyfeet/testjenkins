33b21c0f46700b3831d1d528bba5a4991ce818f7
gh <2495186452@qq.com>
2021-12-14 14:33:32 +0800

    updated

--- testjenkinsson/src/main/java/aa.java
+++ testjenkinsson/src/main/java/aa.java
@@ -7,6 +7,5 @@ public class aa {
     public static void main(String[] args) {
         System.out.println("hello jenkins");
         System.out.println("updated");
-        System.out.println("okhttp");
     }
 }
