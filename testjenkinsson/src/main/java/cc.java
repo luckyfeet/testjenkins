52abeeb12cbee3f16f7e45b0e2644cd9b0939192
gh <2495186452@qq.com>
2021-12-15 10:17:21 +0800

    updated

--- testjenkinsson/src/main/java/cc.java
+++ testjenkinsson/src/main/java/cc.java
@@ -7,5 +7,6 @@ public class cc {
     public static void main(String[] args) {
         System.out.println("alpha");
         System.out.println("missing");
+        System.out.println("zhi");
     }
 }
