ca73c9332d2ef9e3208429e6fac1a54e57597379
gh <2495186452@qq.com>
2021-12-15 10:08:30 +0800

    updated

--- testjenkinsson/src/main/java/cc.java
+++ testjenkinsson/src/main/java/cc.java
@@ -5,8 +5,6 @@
  */
 public class cc {
     public static void main(String[] args) {
-        System.out.println("aaa");
-        System.out.println("bbb");
-        System.out.println("ccc");
+        System.out.println("super");
     }
 }
